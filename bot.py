bac4fe1ae41cfa7578819230afd9e05f95530396
/ Rᴀᴠɪᴅᴜ Yᴀsᴀs / {Oғғʟɪɴᴇ} <slrvtechboy@gmail.com>
2021-12-31 01:02:11 +0845

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ app = Client(
 def start(client, message):
     client.send_message(
         chat_id=message.chat.id,
-        text=f'Hai 😍🙈 ... **{message.from_user.mention}**\n{welcome}",
+        text=f'Hai 😍🙈 ... **{message.from_user.first_name}**\n{welcome}",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
